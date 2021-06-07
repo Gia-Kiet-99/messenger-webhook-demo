ab76ea238468dea25cefd0340f5569435d4fa4b7
Gia-Kiet-99 <kietdg99@gmail.com>
2021-06-07 17:15:40 +0700

    set up env

--- index.js
+++ index.js
@@ -20,26 +20,26 @@ app.get("/", (req, res, next) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  // let VERIFY_TOKEN = "d5f4677de264bb38444452fa8838d56549727ff52f7847a995a1a1a62ad927c8"
-    
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
+
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === Config.VERIFY_TOKEN) {
-      
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
@@ -76,4 +76,5 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('webhook is listening'));
